fc1040259f908fe4f6c5cacf3d4f37a9648e24b0
jisookoh <gosjioo0312@naver.com>
2025-04-08 23:32:54 +0900

    Feat: path 내장 모듈 사용을 위한 @types/node 모듈 devDependencies에 추가

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@common": path.resolve(__dirname, "../../common/src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "components",
+      formats: ["es"],
+      fileName: "components",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
 });
