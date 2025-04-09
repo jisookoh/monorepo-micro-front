b8696187bebc040ca3dc172609ea2be3706fff88
jisookoh <gosjioo0312@naver.com>
2025-04-09 23:01:27 +0900

    Del: vite의 런타임 호환과 next 런타입 호환의 차이로 인한 todo 앱 삭제, next로 새로 생성 예정

--- apps/main/next.config.ts
+++ apps/main/next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 import { NextFederationPlugin } from "@module-federation/nextjs-mf";
-import type { Configuration } from "webpack";
 import withTM from "next-transpile-modules";
 
 const withTranspile = withTM(["@ui"]);
@@ -8,34 +7,21 @@ const withTranspile = withTM(["@ui"]);
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  webpack(config: Configuration, options) {
-    config.plugins = config.plugins || [];
-
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: "main",
-        filename: "remoteEntry.js",
-        remotes: {
-          todo: `promise new Promise(resolve => {
-            const remoteUrl = "http://localhost:3001/assets/remoteEntry.js";
-            const script = document.createElement("script");
-            script.src = remoteUrl;
-            script.type = "module";
-            script.onload = () => {
-              resolve(window.todo);
-            };
-            document.head.appendChild(script);
-          })`,
-        },
-        shared: {
-          react: { singleton: true, eager: true, requiredVersion: false },
-          "react-dom": { singleton: true, eager: true, requiredVersion: false },
-        },
-        extraOptions: {
-          exposePages: false,
-        },
-      })
-    );
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "main",
+          filename: "static/chunks/remoteEntry.js",
+          remotes: {
+            todo: "todo@http://localhost:3001/assets/remoteEntry.js",
+          },
+          extraOptions: {
+            exposePages: false,
+          },
+        })
+      );
+    }
 
     return config;
   },
