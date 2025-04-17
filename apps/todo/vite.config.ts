e7918b35777156afd4cfd224b7b15dd89a280b4f
jisookoh <gosjioo0312@naver.com>
2025-04-18 00:34:57 +0900

    Feat: todo 프로젝트 vite 환경으로 재설치 및 config 설정

---
+++ apps/todo/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "todo",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server: {
+    port: 3001,
+  },
+});
